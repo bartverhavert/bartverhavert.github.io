4de6317b7db73edde840b68dbd4a50e3aef6a244
Bart Verhavert <bart.verhavert@belfius.be>
2015-06-01 07:58:24 +0200

    A date can be displayed.

--- lib/default.rb
+++ lib/default.rb
@@ -1,4 +1,11 @@
+module PostHelper
+  def get_pretty_date(post)
+    attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+  end
+end
+
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
+include PostHelper
