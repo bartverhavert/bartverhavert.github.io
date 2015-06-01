f1339bc1000922f7487cb3e1778f91fa5973b57b
Bart Verhavert <bart.verhavert@icapps.com>
2015-06-01 19:29:41 +0200

    Added a way to preview a post on the home page.

--- lib/default.rb
+++ lib/default.rb
@@ -1,7 +1,21 @@
 module PostHelper
-  def get_pretty_date(post)
-    attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
-  end
+    def get_pretty_date(post)
+        attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+    end
+
+    def get_begin_of_post(post)
+        content = post.compiled_content
+
+        if content =~ /\s<!-- preview_end -->\s/
+            content = content.partition('<!-- preview_end -->').first
+        end
+
+        if content =~ /\s<!-- preview_start -->\s/
+            content = content.partition('<!-- preview_start -->').last
+        end
+
+        return content
+    end
 end
 
 include Nanoc3::Helpers::Blogging
